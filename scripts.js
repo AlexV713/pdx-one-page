401f0f9a2e3ac7d3b17a0c8f1c8a5d299d6164e3
Alejandro Valerio <62916245+AlexV713@users.noreply.github.com>
2022-07-14 14:25:03 -0700

    Added Navigation bars for desktop and mobile

---
+++ scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+    $("#toggle").on("click", function() {
+        $("#mobile-links").toggle(500);  
+        $(".fa-bars").toggleClass("fa-times");
+    });
+
+});
+

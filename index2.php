9112dd58ba123675901ceff848839f5bbe37c6d5
Alejandro Valerio <62916245+AlexV713@users.noreply.github.com>
2022-09-19 16:23:45 -0700

    Added Index2
    I added a new index.php file to begin working on the new style for the website.

---
+++ index2.php
@@ -0,0 +1,40 @@
+<?php require('includes/utilities.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description"
+        content="PDX Handyman Services LLC is a local handyman, renovator, landscaper, and painter in Oregon.">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta property="og:title" content="PDX Handyman Services LLC" />
+    <meta property="og:image" content="https://pdxhandymanservicesllc.com/images/banner.jpg" />
+    <meta property="og:description"
+        content="PDX Handyman Services LLC is a local handyman, renovator, landscaper, and painter in Oregon." />
+    <meta property="og:url" content="https://pdxhandymanservicesllc.com/" />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="628" />
+    <meta property="og:type" content="website" />
+
+    <link href="styles.css" rel="stylesheet" type="text/css">
+    <link rel="canonical" href="https://pdxhandymanservicesllc.com/">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="images/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="images/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicons/favicon-16x16.png">
+    <link rel="manifest" href="images/favicons/site.webmanifest">
+
+    <script src="https://kit.fontawesome.com/cf72e27b3d.js" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <title>PDX Handyman Services LLC</title>
+</head>
+
+<body>
+    <main>
+        
+    </main>
+</body>
+
+</html>

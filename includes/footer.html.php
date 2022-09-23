411b163a39fed478ddd2cb2d3f14dd5a425a64ae
Alejandro Valerio <62916245+AlexV713@users.noreply.github.com>
2022-09-23 11:33:51 -0700

    Moved Header & Footer Links

--- includes/footer.html.php
+++ includes/footer.html.php
@@ -11,8 +11,8 @@
                 <ul>
                     <li><a href="https://pdxhandymanservicesllc.com/">Home</a></li>
                     <li><a href="#services-section">Services</a></li>
-                    <li><a href="#projects-section">Projects</a></li>
                     <li><a href="#about-section">About</a></li>
+                    <li><a href="#projects-section">Projects</a></li>
                     <li><a href="#contact-section">Contact</a></li>
                 </ul>
             </div>
